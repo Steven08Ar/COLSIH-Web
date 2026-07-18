9fc1d3be40644368b7711346e3f5a9b8d67de4fb
Santiago Arias <steven08santi@gmail.com>
2026-07-17 19:43:27 -0500

    Corrige nombre de tabla en modelo PreguntaFrecuente

--- app/Models/PreguntaFrecuente.php
+++ app/Models/PreguntaFrecuente.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PreguntaFrecuente extends Model
 {
+    protected $table = 'preguntas_frecuentes';
+
     protected $fillable = ['pregunta', 'respuesta', 'activo', 'orden'];
 
     protected $casts = ['activo' => 'boolean'];

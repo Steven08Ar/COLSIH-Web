0268f0e9e7c02ae3f4b3206edf93ea28fadc8b6a
Santiago Arias <steven08santi@gmail.com>
2026-08-06 22:36:18 -0500

    fix: agregar meta referrer no-referrer en app.blade.php para evitar bloqueo de Hotlink/CORS de Cloudflare R2 en localhost

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="referrer" content="no-referrer">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/svg+xml" href="/marca/logo-colsih.svg" inertia="favicon">
 
